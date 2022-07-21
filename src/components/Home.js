7dee8c4f57391ddca51ec967e3d11b974a3a983c
Michael Dear <michaelcdear@gmail.com>
2022-07-21 12:22:58 -0600

    commit tree da6f458697b7b5aa2658111204022150feb3e776

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,16 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+
+  const headerStyle = {
+    color: "firebrick"
+  }
+
+  return <div id="home">Home
+  <h1 style={headerStyle}>
+  {name} is a Web Developer from {city}
+  </h1>
+  </div>;
 }
 
 export default Home;
